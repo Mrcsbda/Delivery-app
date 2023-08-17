9890960713f9285fda6f873b25d6d22061bed1a5
mrcsbda <macbedoya@gmail.com>
2023-08-17 16:19:14 -0500

    address styles

--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./feed.scss"
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <article className='feed'>
+      <section className='feed__location-container'>
+        <img src="/images/location.svg" alt="location icon" className='feed__location-icon'/>
+        <div className='feed__address-container'>
+          <p className='feed__address-title'>DELIVER TO</p>
+          <div className='feed__address'>
+            <p className='feed__address-text'>882 Well St, New-York</p>
+            <img src="/images/arrow-down.svg" alt="arrow icon" className='feed__arrow-icon'/>
+          </div>
+        </div>
+      </section>
+    </article>
   )
 }
 
