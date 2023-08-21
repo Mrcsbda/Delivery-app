a73ba86482aded2688979b242e83d955e5d418a6
Mrcsbda <macbedoya@gmail.com>
2023-08-21 10:50:26 -0500

    :lipstick: profile sytles

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "./profile.scss";
 
 const Profile = () => {
-  const [typeOfSettings, setTypeOfSettings] = useState();
-
   const profileSettings = [
     {
       image: "/images/account.svg",
@@ -35,7 +33,7 @@ const Profile = () => {
     },
   ];
 
-  const getTypeOfSettings = (index) => {
+  const typeOfSettings = (index) => {
     switch (index) {
       case 1:
         return "switcher";
@@ -47,24 +45,24 @@ const Profile = () => {
   };
 
   return (
-    <article>
-      <figure>
-        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
-        <figcaption>Alejandra Sanchez</figcaption>
+    <article className="profile">
+      <figure className="profile__picture-container">
+        <img className="profile__picture" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+        <figcaption className="profile__user-name">Alejandra Sanchez</figcaption>
       </figure>
-      <section>
-        <ul>
+      <section className="profile__settings-container">
+        <ul className="profile__settings-list">
           {profileSettings.map((item, index) => {
             return (
-              <li key={index}>
-                <figure>
-                  <img src={item.image} alt="" />
-                  <figcaption>{item.name}</figcaption>
+              <li key={index} className="profile__list-item">
+                <figure className="profile__settings-option-container">
+                  <img src={item.image} alt="" className="profile__settings-option-icon"/>
+                  <figcaption className="profile__settings-option">{item.name}</figcaption>
                 </figure>
-                {getTypeOfSettings(index) ? (
-                  <img src={`/images/${getTypeOfSettings(index)}.svg`} alt="" />
+                {typeOfSettings(index) ? (
+                  <img className="profile__settings-icon" src={`/images/${typeOfSettings(index)}.svg`} alt="" />
                 ) : (
-                  <p>ENG</p>
+                  <p className="profile__language">ENG</p>
                 )}
               </li>
             );
