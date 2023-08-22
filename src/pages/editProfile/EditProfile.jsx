ebcadda120bdd49de57542c1bc67ec68461eb14e
Mrcsbda <macbedoya@gmail.com>
2023-08-22 16:26:42 -0500

    :sparkles: edit profile page

--- src/pages/editProfile/EditProfile.jsx
+++ src/pages/editProfile/EditProfile.jsx
@@ -6,7 +6,23 @@ const EditProfile = () => {
   const [email, setEmail] = useState(true);
   const [number, setNumber] = useState(true);
   const [birthday, setBirthday] = useState(true);
-  const [image, setImage] = useState(true);
+  const [image, setImage] = useState(false);
+
+  const editInfo = (type) => {
+    switch (type) {
+      case "name": setName(!name);
+        break;
+      case "email": setEmail(!email);
+        break;
+      case "number": setNumber(!number);
+        break;
+      case "birthday": setBirthday(!birthday);
+        break;
+      case "image": setImage(!image);
+        break;
+      default: return ""
+    }
+  }
 
   return (
     <article className="edit-profile">
@@ -26,48 +42,49 @@ const EditProfile = () => {
           className="edit-profile__camera-icon"
           src="/images/camera.svg"
           alt="camera icon"
+          onClick={()=> editInfo("image")}
         />
       </figure>
-      <form className="edit-profile__change-image">
-        <input className="edit-profile__input-file" type="file" />
-        <button>Cambiar</button>
+      <form className={`edit-profile__change-image ${image ? "" : "edit-profile__hidden"}`}>
+        <input type="file" />
+        <button type="submit" className="edit-profile__btn-change"> Change </button>
       </form>
       <form className="edit-profile__form">
         <section className="edit-profile__form-container">
-          <div className="edit-profile__input-container">
+          <div className={`edit-profile__input-container ${name ? "" : "edit-profile__edit"}`}>
             <input
               type="text"
               disabled={name ? "disabled" : ""}
               value="Alejandra Sanchez"
             />
-            <img src="/images/edit.svg" alt="edit icon" />
+            <img src="/images/edit.svg" alt="edit icon" onClick={()=> editInfo("name")}/>
           </div>
-          <div className="edit-profile__input-container">
+          <div className={`edit-profile__input-container ${email ? "" : "edit-profile__edit"}`}>
             <input
               type="email"
               disabled={email ? "disabled" : ""}
               value="alejandra@example.com"
             />
-            <img src="/images/edit.svg" alt="edit icon" />
+            <img src="/images/edit.svg" alt="edit icon" onClick={()=> editInfo("email")}/>
           </div>
-          <div className="edit-profile__input-container">
+          <div className={`edit-profile__input-container ${number ? "" : "edit-profile__edit"}`}>
             <input
-              type="number"
+              type="text"
               disabled={number ? "disabled" : ""}
               value="30135536644"
             />
-            <img src="/images/edit.svg" alt="edit icon" />
+            <img src="/images/edit.svg" alt="edit icon" onClick={()=> editInfo("number")}/>
           </div>
-          <div className="edit-profile__input-container">
+          <div className={`edit-profile__input-container ${birthday ? "" : "edit-profile__edit"}`}>
             <input
               type="text"
               disabled={birthday ? "disabled" : ""}
               value="03.05.1995"
             />
-            <img src="/images/edit.svg" alt="edit icon" />
+            <img src="/images/edit.svg" alt="edit icon" onClick={()=> editInfo("birthday")}/>
           </div>
         </section>
-        <button type="submit">Save</button>
+        <button type="submit" className="edit-profile__btn-save">Save</button>
       </form>
     </article>
   );
