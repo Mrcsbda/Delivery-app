8c3ef292fedd5f71b7e61e36e6e3b70a5d86ef5f
Mrcsbda <macbedoya@gmail.com>
2023-08-22 17:14:26 -0500

    :sparkles: add new card

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Layout from './pages/layout/Layout'
 import EditProfile from './pages/editProfile/EditProfile'
+import AddNewCard from './pages/addNewCard/AddNewCard'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<EditProfile/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<AddNewCard/>)
