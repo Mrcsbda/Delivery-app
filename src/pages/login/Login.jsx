691a338db962c78cbe641657107d841ad22c4449
Mrcsbda <macbedoya@gmail.com>
2023-08-25 11:41:40 -0500

    :lipstick: login styles

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./login.scss"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='login'>
+      <img src="/logo.svg" alt="" />
+      <form className='login__form'>
+        <h1 className='login__title'>Login</h1>
+        <input className='login__input' type="email" placeholder='Email' />
+        <input className='login__input' type="password" placeholder='Password' />
+        <div className='login__btns-container'>
+          <button type="submit" className='login__btn-login'>Login</button>
+          <button type="button" className='login__btn-google'>
+            <img src="/images/google.svg" alt="google icon" />
+            <p>Google</p>
+          </button>
+        </div>
+        <p className='login__create-account'>Create account</p>
+      </form>
+    </main>
   )
 }
 
--- src/store/slides/user.js
+++ src/store/slides/user.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    userRole: "CLIENT"
+    userRole: null
 }
 
 export const userSlice = createSlice({
