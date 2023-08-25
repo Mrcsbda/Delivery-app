962901daf70a93c9bc1ff729d8b78ca8db83b2e2
Mrcsbda <macbedoya@gmail.com>
2023-08-25 11:51:34 -0500

    :sparkles: install react hook form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import "./login.scss"
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmit = (data) => {
+
+  }
   return (
     <main className='login'>
       <img src="/logo.svg" alt="" />
-      <form className='login__form'>
+      <form className='login__form' onSubmit={handleSubmit(onSubmit)}>
         <h1 className='login__title'>Login</h1>
-        <input className='login__input' type="email" placeholder='Email' />
-        <input className='login__input' type="password" placeholder='Password' />
+        <input
+          className='login__input'
+          type="email"
+          placeholder='Email' {...register("email", { required: true })} />
+        <input
+          className='login__input'
+          type="password"
+          placeholder='Password' {...register("password", { required: true })} />
         <div className='login__btns-container'>
           <button type="submit" className='login__btn-login'>Login</button>
           <button type="button" className='login__btn-google'>
